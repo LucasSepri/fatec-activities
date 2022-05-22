82e67f0d0ebe52bab16a01c2e5ef7c1fccb5eefa
Lucas Sepriano <lucassepriano@gmail.com>
2022-05-21 22:15:52 -0300

    Add files via upload

---
+++ 2022/ATV C# não lembro do que é/Projeto1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercises_Csharp
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			/*Utilizar comandos de cor de fundo, cor de letra e moldura.
+
+1 – Criar um programa que permita a digitação do salário mínimo e do salário do usuário, calcular quantos salários mínimos a pessoa ganha.
+
+2 – Elaborar um programa que permita que o usuário digite uma temperatura em graus Celsius e o programa converta para Kelvin.
+
+3 - Escreva um algoritmo para ler uma temperatura em graus Fahrenheit, calcular e escrever o valor correspondente em graus Celsius 
+
+4 – Criar um programa que permita que o usuário digite seu peso em kg, calcular e mostrar:
+
+Peso em gramas.
+O peso com um acréscimo de 10%
+
+			1)*/
+		int min, at , r
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			Console.ReadKey();
+		}
+	}
+}
